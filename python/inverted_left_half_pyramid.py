1698532f014015f629cb2554687e43797842b1a0
Anvith V K <anvithvk2002@gmail.com>
2023-12-01 21:34:43 +0530

    Update inverted_left_half_pyramid.py

--- python/inverted_left_half_pyramid.py
+++ python/inverted_left_half_pyramid.py
@@ -1,7 +1,10 @@
 rows = 5
 
 for i in range(rows, 0, -1):
-    for j in range(i):
+    for j in range(rows - i):
+        print(" ", end=" ")
+
+    for k in range(i):
         print("*", end=" ")
 
     print()
